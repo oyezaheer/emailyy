a81a2b993d412ef3b1b520ae765859dcab22306e
oyezaheer <zaheer286khan@gmail.com>
2023-09-23 11:48:18 +0530

    You S

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({bye : 'buddy'});
+    res.send({bye : 'haan bhai aa gya sswadd'});
 });
 
 const PORT = process.env.PORT || 5000;
