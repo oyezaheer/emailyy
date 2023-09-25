95712924f9a5f8e73667313edb103e65653144a4
oyezaheer <zaheer286khan@gmail.com>
2023-09-26 02:50:10 +0530

    You

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret : keys.googleClientSecret,
-            callbackURL: "http://localhost:5000/auth/google/callback",
+            callbackURL: "/auth/google/callback",
             proxy: true
             // passReqToCallback: true
         },
