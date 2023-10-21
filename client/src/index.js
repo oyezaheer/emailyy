aeb7ee751592b084713a964d51b9680a20b9e662
adityabanduke <adityabanduke@gmail.com>
2023-10-22 00:32:12 +0530

    Added billing and client side app!

--- client/src/index.js
+++ client/src/index.js
@@ -10,21 +10,11 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-
-
-
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-
-
 root.render(
     <Provider store={store}><App /></Provider>
 ); 
-
-
-console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
-console.log('Environment is', process.env.NODE_ENV);
