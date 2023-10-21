60bb0aa2aef7a05b505be0e2b313e7e547d9f382
adityabanduke <adityabanduke@gmail.com>
2023-10-22 00:44:57 +0530

    on render

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-// const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
@@ -10,8 +9,9 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
-
 const app = express();
+app.use(bodyParser.json());
+
 app.use(
     session({
         secret:"Our little secret",
@@ -20,16 +20,23 @@ app.use(
     })
 );
 
-app.use(bodyParser.json());
 
-// app.use(cookieSession( {
-//     maxAge:30 * 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieKey]
-// })
-// );
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+if(process.env.NODE_ENV === 'production'){
+    // Express will serve up production assets
+    // like our main.js file, or main.css file
+    app.use(express.static('client/build'));
+    
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 
 require('./routes/authRoutes')(app);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,15 +1,44 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
+
+
+
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: 'usd',
-      description: '$5 for 5 credits',
-      source: req.body.id
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    const product = await stripe.products.create({
+      name: "harddd"
+      // images: productImages
+      });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          unit_amount: 500,
+          currency: 'usd',
+          // description: '$5 for 5 credits',
+          product: product.id
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
     });
 
-    console.log(charge);
+    // const charge = await stripe.charges.create({
+    //   amount: 500,
+    //   currency: 'usd',
+    //   description: '$5 for 5 credits',
+    //   source: req.body.id
+    // });
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+    // console.log(charge);
+    console.log("user",user)
+
+    return res.send(user);
   });
 };
