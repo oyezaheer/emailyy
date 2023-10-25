cfd16399a74710846eb5a6625b194eb2ab82a4f5
adityabanduke <adityabanduke@gmail.com>
2023-10-25 19:00:27 +0530

    final commit for all modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MemoryStore = require('memorystore')(session);
+// const MemoryStore = require('memorystore')(session);
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
@@ -21,14 +21,14 @@ app.use(
     })
 );
 
-app.use(session({
-    cookie: { maxAge: 86400000 },
-    store: new MemoryStore({    
-        checkPeriod: 86400000 // prune expired entries every 24h
-    }),
-    resave: false,
-    secret: 'keyboard cat'
-}))
+// app.use(session({
+//     cookie: { maxAge: 86400000 },
+//     store: new MemoryStore({    
+//         checkPeriod: 86400000 // prune expired entries every 24h
+//     }),
+//     resave: false,
+//     secret: 'keyboard cat'
+// }))
 
 
 app.use(passport.initialize());
