d677dc712bcb331fa2cd01df5be7026e83c5bd42
adityabanduke <adityabanduke@gmail.com>
2023-10-25 11:40:11 +0530

    Secret Session

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
@@ -20,6 +21,14 @@ app.use(
     })
 );
 
+app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({    
+        checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: 'keyboard cat'
+}))
 
 
 app.use(passport.initialize());
@@ -29,7 +38,7 @@ if(process.env.NODE_ENV === 'production'){
     // Express will serve up production assets
     // like our main.js file, or main.css file
     app.use(express.static('client/build'));
-    
+
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     const path = require('path');
